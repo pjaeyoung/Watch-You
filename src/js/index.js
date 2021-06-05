da95801ba2b93f3df9f8940ffc1e4b32df4e760a
pjaeyoung <devdodose@gmail.com>
2021-06-06 00:51:19 +0900

    feat: 엘리먼트 표시 $ 추가 및 신호등 불 5초마다 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
-const screenStart = document.querySelector("#screen-start");
-const screenGame1 = document.querySelector("#screen-game-1");
-let currentScreen = screenStart;
+const $screenStart = document.querySelector("#screen-start");
+const $screenGame1 = document.querySelector("#screen-game-1");
+let $currentScreen = $screenStart;
+
+const $traffigLight = document.querySelector("#traffic-light");
+let trafficLightTimer;
+function switchTrafficLightTimer() {
+  $traffigLight.classList.toggle("active");
+}
 
 document.querySelector("#btn-start").addEventListener("click", () => {
-  screenGame1.classList.add("active");
-  currentScreen.classList.remove("active");
-  currentScreen = screenGame1;
+  $screenGame1.classList.add("active");
+  $currentScreen.classList.remove("active");
+  $currentScreen = $screenGame1;
+  trafficLightTimer = setInterval(switchTrafficLightTimer, 5000);
 });
